413f9e02a7d38b45c4bcfb227cc2db27ca4a2806
ishitakarna <ishitakarna@gmail.com>
2021-09-23 10:25:35 +0530

    Initial commit

---
+++ src/main/java/com/hackathon/iplbidding/IplbiddingApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.iplbidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplbiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplbiddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/iplbidding/IplbiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.iplbidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplbiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
